c584b63e4ecf1331fda4d1bce4aad7be1fe1a399
amirhosseinasa <amirhossein.asa.pro@gmail.com>
2023-03-07 22:14:21 +0330

    initial commit

---
+++ Assets/Scripts/AI/parse_specification.py
@@ -0,0 +1,155 @@
+# Generates the code from a decision tree specification
+
+import sys
+import yaml
+import re
+import os
+
+funcs = {
+    'abs\(': 'Math.Abs(',
+    'min\(': 'Math.Min(',
+    'max\(': 'Math.Max(',
+}
+
+# replaces expressions and functions
+def parse_code(line):
+    #expressions
+    for key, value in EXPRESSIONS.items():
+        line = re.sub(key, str(value), line)
+
+    #functions
+    for name, repl in funcs.items():
+        line = re.sub(name, repl, line)
+
+    if ';' in line: # multiple instructions
+        return '{' + line + '}'
+    else:
+        return line
+
+# forms the decision tree code
+def create_tree(name):
+    global CREATED_NODES
+    if name not in CREATED_NODES:
+        code_lines = []
+        node = NODES[name]
+
+        # node with just the action
+        if type(node) is str:
+            node = {'action': node}
+
+        #left children
+        if True in node:
+            yes_nodes = [x.strip() for x in node[True].split(';')]
+            for yes_node in yes_nodes:
+                code_lines += create_tree(yes_node)
+            left = 'new List<DecisionTree> {' + ', '.join(yes_nodes) + '}'
+        else:
+            left = 'null'
+
+        # right children
+        if False in node:
+            no_nodes = [x.strip() for x in node[False].split(';')]
+            for no_node in no_nodes:
+                code_lines += create_tree(no_node)
+            right = 'new List<DecisionTree> {' + ', '.join(no_nodes) + '}'
+        else:
+            right = 'null'
+
+
+        #action
+        if 'action' in node:
+            action = f'(state, data) => {parse_code(node["action"])}'
+        else:
+            action = 'null'
+
+        #decision
+        if 'decision' in node:
+            decision = f'(state, data) => {parse_code(node["decision"])}'
+        else:
+            decision = 'null'
+
+        #comment
+        if 'info' in node:
+            info = f'// {node["info"]}\n\t\t'
+        else:
+            info = ''
+
+
+        # add to created nodes
+        CREATED_NODES.add(name)
+
+        code_lines.append(f'{info}DecisionTree {name} = new DecisionTree("{name}", {action}, {decision}, {left}, {right});\n')
+        return code_lines
+    
+    else:
+        return []
+
+
+# process included files
+def process_include(filenames):
+    for filename in filenames:
+        with open(filename) as file:
+            spec = yaml.safe_load(file)
+            exps = spec['expressions'] if 'expressions' in spec else {}
+            nds = spec['nodes'] if 'nodes' in spec else {}
+            incs = spec['include'] if 'include' in spec else []
+            # expressions
+            for name, exp in exps.items():
+                if name not in EXPRESSIONS:
+                    EXPRESSIONS[name] = exp
+            # nodes
+            for name, node in nds.items():
+                if name not in NODES:
+                    NODES[name] = node
+            
+            #other includes
+            process_include([FOLDER_PATH + inc for inc in incs])
+
+
+# main (receives the filename as argument)
+def main():
+    with open(sys.argv[1]) as file:
+        spec = yaml.safe_load(file)
+        global FOLDER_PATH
+        FOLDER_PATH = os.path.dirname(file.name) + '/'
+        name = os.path.splitext(os.path.basename(file.name))[0].capitalize() + 'Tree'
+
+    global EXPRESSIONS
+    EXPRESSIONS = spec['expressions'] if 'expressions' in spec else {}
+
+    global NODES
+    NODES = spec['nodes']
+
+    include = spec['include'] if 'include' in spec else {}
+    process_include([FOLDER_PATH + i for i in include])
+
+    global CREATED_NODES
+    CREATED_NODES = set()
+
+    tree_code = '\n\t\t'.join(create_tree('ROOT'))
+
+    code = f'''
+using System;
+using System.Collections.Generic;
+
+//Generated code
+class {name} {{
+
+    public static DecisionTree CreateTree() {{
+
+        {tree_code}
+        return ROOT;
+    }}
+}}
+'''
+
+    if not os.path.exists('generated_code'):
+        os.mkdir('generated_code')
+
+    out_path = 'generated_code/' + name + '.cs'
+    with open(out_path, 'w') as file:
+        file.write(code)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assets/Scripts/AI/plot_specification.py
@@ -0,0 +1,109 @@
+# Creates an image representation of a decision tree
+
+from networkx import DiGraph, draw, draw_networkx_labels, draw_networkx_edge_labels
+from networkx.drawing.nx_agraph import graphviz_layout
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import sys
+import yaml
+import os
+
+YES_COLOR = '#5cf873'
+NO_COLOR = '#f85c79'
+YES_NO_COLOR = '#f5ec42'
+
+# forms the tree in a graph
+def build_tree(root_name):
+    if not G.has_node(root_name):
+        node = NODES[root_name]
+        G.add_node(root_name)
+
+        if type(node) is str:
+            node = {'action': node}
+
+        if True in node:
+            for n in node[True].split(';'):
+                n = n.strip()
+                build_tree(n)
+                G.add_edge(root_name, n, color=YES_COLOR)
+                edge_labels[(root_name, n)] = 'yes'
+
+        if False in node:
+            for n in node[False].split(';'):
+                n = n.strip()
+                build_tree(n)
+                # edge in both yes and no
+                if (G.has_edge(root_name, n)):
+                    G.add_edge(root_name, n, color=YES_NO_COLOR)
+                    edge_labels[(root_name, n)] = 'yes/no'
+                else:
+                    G.add_edge(root_name, n, color=NO_COLOR)
+                    edge_labels[(root_name, n)] = 'no'
+
+        if 'info' in node:
+            node_labels[root_name] = '\n\n\n' + node['info']
+        else:
+            node_labels[root_name] = ''
+
+
+# process include files
+def process_include(filenames):
+    for filename in filenames:
+        with open(filename) as file:
+            spec = yaml.safe_load(file)
+            nds = spec['nodes'] if 'nodes' in spec else {}
+            incs = spec['include'] if 'include' in spec else []
+            
+            # nodes
+            for name, node in nds.items():
+                if name not in NODES:
+                    NODES[name] = node
+            
+            #other includes
+            process_include([FOLDER_PATH + inc for inc in incs])
+
+
+# main (receives the filename, width and height as argument)
+def main():
+    with open(sys.argv[1]) as file:
+        spec = yaml.safe_load(file)
+        global FOLDER_PATH
+        FOLDER_PATH = os.path.dirname(file.name) + '/'
+        name = os.path.splitext(os.path.basename(file.name))[0].capitalize() + 'Tree'
+
+    fig_width = float(sys.argv[2])
+    fig_height = float(sys.argv[3])
+
+    global NODES
+    NODES = spec['nodes']
+
+    include = spec['include'] if 'include' in spec else {}
+    process_include([FOLDER_PATH + i for i in include])
+    
+    global edge_labels, node_labels
+    edge_labels = {}
+    node_labels = {}
+
+    global G
+    G = DiGraph()
+    build_tree('ROOT')
+    
+    edges = G.edges()
+    colors = [G[u][v]['color'] for u, v in edges]
+    pos = graphviz_layout(G, prog='dot')
+
+    plt.figure(figsize=(fig_width, fig_height))
+    draw(G, pos, with_labels=True, arrows=True, node_size=0,
+         width=1, node_color='#333333', edge_color=colors, font_size=11)
+    draw_networkx_labels(G, pos, labels=node_labels, font_size=7, font_color='#666666')
+    draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6)
+
+    if not os.path.exists('plots'):
+        os.mkdir('plots')
+
+    plt.savefig('plots/' + name + '.png', dpi=300)
+
+
+if __name__ == "__main__":
+    main()
